223db3ecd3ea75464d8d62f3362134c1cc8c682d
Mário Freitas <imkira@gmail.com>
2015-03-08 17:26:41 +0900

    modified: test refactoring

--- stream_test.go
+++ stream_test.go
@@ -3,76 +3,64 @@ package observer
 import "testing"
 
 func TestStreamInitialValue(t *testing.T) {
-	state1 := newState(10)
-	stream1 := &stream{state: state1}
-	if val := stream1.Value(); val != 10 {
+	state := newState(10)
+	stream := &stream{state: state}
+	if val := stream.Value(); val != 10 {
 		t.Fatalf("Expecting 10 but got %#v\n", val)
 	}
 }
 
 func TestStreamUpdate(t *testing.T) {
 	state1 := newState(10)
-	stream1 := &stream{state: state1}
-	select {
-	case <-stream1.Changes():
-		t.Fatalf("Expecting no changes\n")
-	default:
-	}
 	state2 := state1.update(15)
-	stream2 := &stream{state: state2}
-	select {
-	case <-stream2.Changes():
-		t.Fatalf("Expecting no changes\n")
-	default:
+	stream := &stream{state: state1}
+	if val := stream.Value(); val != 10 {
+		t.Fatalf("Expecting 10 but got %#v\n", val)
 	}
-	if val := stream2.Value(); val != 15 {
-		t.Fatalf("Expecting 15 but got %#v\n", val)
-	}
-	select {
-	case <-stream1.Changes():
-	default:
-		t.Fatalf("Expecting changes\n")
-	}
-	stream1.Next()
-	if val := stream1.Value(); val != 15 {
-		t.Fatalf("Expecting 15 but got %#v\n", val)
+	state2.update(15)
+	if val := stream.Value(); val != 10 {
+		t.Fatalf("Expecting 10 but got %#v\n", val)
 	}
 }
 
-func TestStreamNextReturnsValue(t *testing.T) {
+func TestStreamNextValue(t *testing.T) {
 	state1 := newState(10)
-	stream1 := &stream{state: state1}
-	state1.update(15)
-	if val := stream1.Next(); val != 15 {
+	stream := &stream{state: state1}
+	state2 := state1.update(15)
+	if val := stream.Next(); val != 15 {
 		t.Fatalf("Expecting 15 but got %#v\n", val)
+	}
+	state2.update(20)
+	if val := stream.Next(); val != 20 {
+		t.Fatalf("Expecting 20 but got %#v\n", val)
 	}
 }
 
-func TestStreamDetectsChanges(t *testing.T) {
-	state1 := newState(10)
-	stream1 := &stream{state: state1}
-	if has := stream1.HasNext(); has {
+func TestStreamHasChanges(t *testing.T) {
+	state := newState(10)
+	stream := &stream{state: state}
+	if stream.HasNext() {
 		t.Fatalf("Expecting no changes\n")
 	}
-	state1.update(15)
-	if has := stream1.HasNext(); !has {
+	state.update(15)
+	if !stream.HasNext() {
 		t.Fatalf("Expecting changes\n")
 	}
-	if val := stream1.Next(); val != 15 {
+	if val := stream.Next(); val != 15 {
 		t.Fatalf("Expecting 15 but got %#v\n", val)
 	}
 }
 
 func TestStreamWaitsNext(t *testing.T) {
-	state1 := newState(10)
-	stream1 := &stream{state: state1}
+	state := newState(10)
+	stream := &stream{state: state}
 	for i := 15; i <= 100; i++ {
-		state1 = state1.update(i)
-		if val := stream1.WaitNext(); val != i {
+		state = state.update(i)
+		if val := stream.WaitNext(); val != i {
 			t.Fatalf("Expecting %#v but got %#v\n", i, val)
 		}
 	}
-	if has := stream1.HasNext(); has {
+	if stream.HasNext() {
 		t.Fatalf("Expecting no changes\n")
 	}
 }
